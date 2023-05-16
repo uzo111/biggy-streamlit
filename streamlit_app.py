860b0175fc5be5fd7101343a35a4d978d512931d
BIGGY <92266060+uzo111@users.noreply.github.com>
2023-05-16 15:46:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 # features list
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
     'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
+
 # Give a title to web app using html syntax
 st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
 
@@ -95,7 +96,8 @@ def main():
        [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
-       a,b,c = st.columns([0.2,0.6,0.2])
+
+a,b,c = st.columns([0.2,0.6,0.2])
 with b:
  st.image("banner-picture.jpeg", use_column_width=True)
 
